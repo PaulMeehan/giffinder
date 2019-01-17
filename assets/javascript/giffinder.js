6d13a62de21dbd2bac8140a0d107ea82b84a772a
PaulM <pmeehan456@gmail.com>
2019-01-16 22:44:02 -0500

    API call working

--- assets/javascript/giffinder.js
+++ assets/javascript/giffinder.js
@@ -1,9 +1,32 @@
 
+var topics = ["Steak", "Pork", "Chicken", "Potato", "Carrots"];
 
-// var xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=&q=cheese&limit=10&offset=0&rating=G&lang=en");
-// xhr.done(function (data) { console.log("success got data", data); });
+function swapImage (clickedIndex) {
+    var imageID = "image" + clickedIndex;
+    var currentSource = $("#" + imageID).attr("src");
+    var newSource = "";
+    if (currentSource.substring(currentSource.length - 6) === "_s.gif") {
+        newSource = currentSource.substring(0,currentSource.length - 6) + ".gif";
+    } else {
+        newSource = currentSource.substring(0,currentSource.length - 4) + "_s.gif"
+    };
+    $("#" + imageID).attr("src",newSource);
+};
 
 
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=jlyk6tnm9IH9Jd6K9eQTrQneGfOmXT3V&limit=5");
-xhr.done(function (data) { console.log("success got data", data); });
+$("#testbutton").click(function () {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jlyk6tnm9IH9Jd6K9eQTrQneGfOmXT3V&q=cheese&limit=10&offset=0&rating=G&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        for (i = 0; i < response.data.length; i++) {
+            $("#left").append("<img src='" + response.data[i].images.fixed_height_still.url + "' id='image" + i + "' onclick='swapImage(" + i +")'>");
+        };
+    });
+});
+
