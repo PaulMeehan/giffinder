5b9b0b19035f4437f64147cd1411938543cade88
PaulM <pmeehan456@gmail.com>
2019-01-18 22:25:26 -0500

    fully functioning

--- assets/javascript/giffinder.js
+++ assets/javascript/giffinder.js
@@ -1,23 +1,24 @@
 
-var topics = ["Steak", "Pork", "Chicken", "Potato", "Carrots"];
+var topics = ["Steak", "Pork", "Chicken", "Potato", "Carrots", "Cheese"];
 
-function swapImage (clickedIndex) {
-    var imageID = "image" + clickedIndex;
-    var currentSource = $("#" + imageID).attr("src");
-    var newSource = "";
-    if (currentSource.substring(currentSource.length - 6) === "_s.gif") {
-        newSource = currentSource.substring(0,currentSource.length - 6) + ".gif";
+function swapImage (currentImg) {
+    var src = $(currentImg).attr("src");
+    var still = $(currentImg).attr("data-stillImg");
+    var animated = $(currentImg).attr("data-animatedImg");
+    
+    if (src === still) {
+        $(currentImg).attr("src",animated);
     } else {
-        newSource = currentSource.substring(0,currentSource.length - 4) + "_s.gif"
+        $(currentImg).attr("src", still);
     };
-    $("#" + imageID).attr("src",newSource);
 };
 
 
+function showSubjects () {
+    var subject = $(this).attr("id");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jlyk6tnm9IH9Jd6K9eQTrQneGfOmXT3V&q=" + subject + "&limit=10&offset=0&rating=G&lang=en";
 
-$("#testbutton").click(function () {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jlyk6tnm9IH9Jd6K9eQTrQneGfOmXT3V&q=cheese&limit=10&offset=0&rating=G&lang=en";
-
+    $("#left").empty();
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -25,8 +26,35 @@ $("#testbutton").click(function () {
         console.log(response);
 
         for (i = 0; i < response.data.length; i++) {
-            $("#left").append("<img src='" + response.data[i].images.fixed_height_still.url + "' id='image" + i + "' onclick='swapImage(" + i +")'>");
+            var newImage = $("<img>");
+            newImage.attr("src", response.data[i].images.fixed_height_still.url);
+            newImage.attr("data-stillImg", response.data[i].images.fixed_height_still.url);
+            newImage.attr("data-animatedImg", response.data[i].images.fixed_height.url);
+            newImage.attr("onclick", "swapImage(this)");
+
+            $("#left").append(newImage);
         };
     });
-});
+};
 
+$(document).on("click", ".subjectButton", showSubjects);
+
+
+function addButton (newSubject) {
+    var newButton = $("<button>");
+    newButton.attr("type","button");
+    newButton.attr("id",newSubject);
+    newButton.text(newSubject);
+    newButton.attr("class", "subjectButton btn btn-primary");
+    $("#subjectMenu").append(newButton);
+};
+
+
+for (i=0; i < topics.length; i++) {
+    addButton(topics[i]);
+};
+
+function addUserButton () {
+    var newText = $("#subject-input").val();
+    addButton(newText);
+};
